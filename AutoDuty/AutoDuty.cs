bf101f00dbf2319ca1713b160f3fc211fe4e60fc
Simon Latusek <Simon.Latusek@gmail.com>
2026-03-23 16:34:18 +0100

    Updating Wrath, Dala, ECommons

--- AutoDuty/AutoDuty.cs
+++ AutoDuty/AutoDuty.cs
@@ -292,7 +292,7 @@ public sealed class AutoDuty : IDalamudPlugin
             this.assemblyDirectoryInfo = this.assemblyFileInfo.Directory;
 
             this.Version = 
-                ((PluginInterface.IsDev     ? new Version(0,0,0, 296) :
+                ((PluginInterface.IsDev     ? new Version(0,0,0, 298) :
                   PluginInterface.IsTesting ? PluginInterface.Manifest.TestingAssemblyVersion ?? PluginInterface.Manifest.AssemblyVersion : PluginInterface.Manifest.AssemblyVersion)!).Revision;
 
             if (!this.configDirectory.Exists)
